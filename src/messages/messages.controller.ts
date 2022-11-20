878e8cf21d9a416b000db3ef77ae5f454692e928
Christian <christiansantos.dev@gmail.com>
2022-11-20 20:29:17 -0300

    feat: add findall messages endpoint

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  findAll() {
+    return [
+      { id: 1, text: 'macaco' },
+      { id: 2, text: 'ugagua' },
+    ];
+  }
+}
